0b0dd75386b16d6935a158cee3ce1ece8f4a0bae
Altaf <safi.mohammad.altaf@gmail.com>
2024-08-01 20:59:18 -0500

    Selenium Utility class

--- src/test/java/tek/bdd/utility/SeleniumUtility.java
+++ src/test/java/tek/bdd/utility/SeleniumUtility.java
@@ -1,6 +1,7 @@
 package tek.bdd.utility;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import tek.bdd.base.BaseSetup;
@@ -18,9 +19,18 @@ public class SeleniumUtility extends BaseSetup {
         getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
 
     }
+    private WebElement waitForVisibility(By locator){
+      return   getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+    }
 
     public void SendText(By locator, String value){
-        getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys("value");
+        waitForVisibility(locator).sendKeys(value);
+
+    }
+
+    public String getElementText(By locator){
+       return waitForVisibility(locator).getText();
 
     }
 }
