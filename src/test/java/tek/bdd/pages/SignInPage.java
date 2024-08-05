8d6e36ba1e095540152ecadb369852f704bb5ae0
Altaf <safi.mohammad.altaf@gmail.com>
2024-08-05 04:50:20 -0500

    Selenium BDD framework Setup progress 08/05/2024

---
+++ src/test/java/tek/bdd/pages/SignInPage.java
@@ -0,0 +1,11 @@
+package tek.bdd.pages;
+
+import org.openqa.selenium.By;
+
+public class SignInPage {
+    public static final By SIGN_IN_PAGE_TEXT_TITLE = By.className("login__subtitle");
+    public static final By EMAIL_INPUT = By.name("email");
+    public static final By PASSWORD_INPUT = By.name("password");
+    public static final By LOGIN_BUTTON = By.id("loginBtn");
+
+}
--- src/test/java/tek/bdd/steps/SecurityTestSteps.java
+++ src/test/java/tek/bdd/steps/SecurityTestSteps.java
@@ -4,19 +4,21 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import tek.bdd.pages.HomePage;
+import tek.bdd.pages.SignInPage;
 import tek.bdd.utility.SeleniumUtility;
 
 public class SecurityTestSteps extends SeleniumUtility {
 
     @When("user click on sign in link")
     public void validate_user_is_in_sign_in_page() {
-            ClickOnElement(By.id("signinLink"));
+            ClickOnElement(HomePage.SIGN_IN_LINK);
     }
 
 
     @Then("validate user is in sign in page")
     public void user_enter_and_and_click_on_login() {
-          String PageTextTitle =  getElementText(By.className("login__subtitle"));
+          String PageTextTitle =  getElementText(SignInPage.SIGN_IN_PAGE_TEXT_TITLE);
         Assert.assertEquals("Sign in",PageTextTitle);
     }
 
@@ -24,14 +26,14 @@ public class SecurityTestSteps extends SeleniumUtility {
 
     @When("user enter \"mohammad2536@gmail.com\" and \"Password@123\" and click on login")
     public void user_should_be_able_to_see_account_link() {
-        SendText(By.name("email"),"mohammad2536@gmail.com");
-        SendText(By.name("password"),"Password@123");
-        ClickOnElement(By.id("loginBtn"));
+        SendText(SignInPage.EMAIL_INPUT,"mohammad2536@gmail.com");
+        SendText(SignInPage.PASSWORD_INPUT,"Password@123");
+        ClickOnElement(SignInPage.LOGIN_BUTTON);
 
     }
     @Then("user should be able to see account link")
     public void User_Should_Be_Able_To_See_Account(){
-       boolean isAccountDisplayed = isElementDisplayed(By.id("accountLink"));
+       boolean isAccountDisplayed = isElementDisplayed(HomePage.ACCOUNT_LINK);
        Assert.assertTrue(isAccountDisplayed);
     }
 }
